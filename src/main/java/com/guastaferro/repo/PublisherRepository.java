c8f9ca3c30108b25d218599edd9bf00bee319aeb
Andrea Guastaferro <andrea.guastaferro@soprasteria.com>
2020-09-18 15:08:43 +0200

    feature/TestAddEntityPublisher

---
+++ src/main/java/com/guastaferro/repo/PublisherRepository.java
@@ -0,0 +1,8 @@
+package com.guastaferro.repo;
+
+import com.guastaferro.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher,Long> {
+
+}
